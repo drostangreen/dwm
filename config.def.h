6a07362c3ec9518bb0abdaddb3e1bd1e260a9d97
drostangreen <t.w.green29@gmail.com>
2023-01-21 21:17:52 -0600

    Added custom keys for different apps, tag icons

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -60,11 +60,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+/* custom commands */
+static const char *code[]  = { "code", NULL };
+static const char *discord[]  = { "discord", NULL };
+static const char *firefox[]  = { "firefox", NULL };
+static const char *brave[]  = { "brave", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = discord } },
+	{ MODKEY,                       XK_c,      spawn,          {.v = code } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefox } },
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = brave } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
